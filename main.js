6c58a7993f1d52236ef72f927c70b80b3340e131
Jeremy Smith <jmsmith57@hotmail.com>
2017-02-13 13:22:39 +0000

    added new viws and rearranged the website

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-$(document).ready(function(){
-
-  $(document).on('click', 'a', function(event){
-    event.preventDefault();
-
-    $('html, body').animate({
-      scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
   });
 });
